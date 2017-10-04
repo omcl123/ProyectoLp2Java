8031799c955035b31be454902ae82b2247ac5a9d
Oscar <Oscar@DESKTOP-44C66HF>
2017-10-04 16:48:47 -0500

    Modificacion vista de Auditorias

--- JavaApplication3/src/Modelo/Anuncio.java
+++ JavaApplication3/src/Modelo/Anuncio.java
@@ -5,10 +5,67 @@
  */
 package Modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Oscar
  */
 public class Anuncio {
+
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private Date fechacreacion;
+    private Docente docenteCreador;
+    private Curso curso;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public void setFechacreacion(Date fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    public Docente getDocenteCreador() {
+        return docenteCreador;
+    }
+
+    public void setDocenteCreador(Docente docenteCreador) {
+        this.docenteCreador = docenteCreador;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
     
 }
