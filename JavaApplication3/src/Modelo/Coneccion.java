62bf92928b3c9a26bc68bdb2e52d431306acb488
Oscar <Oscar@DESKTOP-44C66HF>
2017-10-31 21:50:21 -0500

    Clase conexion con metodos y creacion de auditoriaBL y auditoriaDA

--- JavaApplication3/src/Modelo/Coneccion.java
+++ JavaApplication3/src/Modelo/Coneccion.java
@@ -26,10 +26,7 @@ public class Coneccion {
             con.close();
         }catch (Exception e){}
     }
-    public Statement createStatement(){
-        try{
-            return con.createStatement();
-        }catch (Exception e);
-        
+    public Statement createStatement()throws Exception{
+        return con.createStatement();
     }
 }
