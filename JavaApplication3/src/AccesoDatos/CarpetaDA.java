10aa83229e02d26db6c649508ffe09181a97b2bb
Oscar <Oscar@DESKTOP-44C66HF>
2017-11-06 18:27:59 -0500

    Creacion CarpetaDA

--- JavaApplication3/src/AccesoDatos/CarpetaDA.java
+++ JavaApplication3/src/AccesoDatos/CarpetaDA.java
@@ -5,8 +5,10 @@
  */
 package AccesoDatos;
 import Modelo.Carpeta;
+import Modelo.Curso;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -47,5 +49,59 @@ public class CarpetaDA {
            
         }
         return lista;
+    }
+    public void registraCarpeta(Carpeta c,int opcion){
+        CursoDA cDA=new CursoDA();
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        con = (com.mysql.jdbc.Connection)DriverManager.getConnection("jdbc:mysql://200.16.7.96/inf282g5", 
+                    "inf282g5", "reFuKUxhUijfr8np");
+        }catch(Exception e){System.out.println("fallo en coneccion ");}
+        
+        try{
+            int nextId=cDA.getNextId();
+            Statement sentencia = con.createStatement();
+            if(opcion==1){
+                //System.out.println("creando carpeta ");
+                String instruccion = "INSERT INTO Carpeta VALUES (?,?,?,sysdate(),?,?,0,1);";
+                PreparedStatement ps=con.prepareStatement(instruccion);
+                ps.setInt(1, nextId);
+                ps.setInt(2, c.getCarpetaPadre());
+                ps.setString(3, c.getNombre());
+                ps.setString(4, c.getDescripcion());
+                ps.setInt(5, c.getCurso());
+                ps.execute();
+                //int i=sentencia.executeUpdate(instruccion);
+            }
+            else{
+                String instruccion = "INSERT INTO Carpeta VALUES (?,?,?,sysdate(),?,NULL,0,1);";
+                PreparedStatement ps=con.prepareStatement(instruccion);
+                ps.setInt(1, nextId);
+                ps.setInt(2, c.getCarpetaPadre());
+                ps.setString(3, c.getNombre());
+                ps.setString(4, c.getDescripcion());
+                ps.execute();
+            }
+            con.close();
+        }catch(Exception e){
+           System.out.println("fallo en query ");
+        }
+    }
+    public void eliminarCarpeta(Carpeta c){
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        con = (com.mysql.jdbc.Connection)DriverManager.getConnection("jdbc:mysql://200.16.7.96/inf282g5", 
+                    "inf282g5", "reFuKUxhUijfr8np");
+        }catch(Exception e){System.out.println("fallo en coneccion ");}
+        
+        try{
+            String instruccion = "DELETE FROM Carpeta WHERE id = ?;";
+            PreparedStatement ps=con.prepareStatement(instruccion);
+            ps.setInt(1, c.getId());
+            ps.execute();
+            con.close();
+        }catch(Exception e){
+           System.out.println("fallo en query ");
+        }
     }
 }
